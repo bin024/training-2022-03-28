723baf51d1f36da223467b204c46e78db6d2c7e1
bin024 <100820966+bin024@users.noreply.github.com>
2022-04-06 00:51:19 -0400

    Add files via upload

---
+++ HW4/HW4.js
@@ -0,0 +1,6 @@
+function getLetterName(){
+    document.getElementById('letter').value = document.getElementById('fruit').value;
+}
+function getFruitName(){
+    document.getElementById('fruit').value = document.getElementById('letter').value;
+}
